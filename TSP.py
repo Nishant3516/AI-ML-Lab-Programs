847ba16b4c0eff553080eff0cb62287823a1e621
Nishant Bansal <81610684+Nishant3516@users.noreply.github.com>
2023-10-16 01:09:41 +0530

    Add files via upload

--- TSP.py
+++ TSP.py
@@ -1,52 +1,38 @@
-class City:
-    def __init__(self, name, x, y):
-        self.name = name
-        self.x = x
-        self.y = y
-        self.visited = False
+class TravelingSalesman:
+    def __init__(self, distance_matrix):
+        self.distance_matrix = distance_matrix
+        self.num_cities = len(distance_matrix)
 
-    def distance_to(self, other_city):
-        return ((self.x - other_city.x) ** 2 + (self.y - other_city.y) ** 2) ** 0.5
+    def nearest_neighbor_tsp(self):
+        unvisited_cities = set(range(self.num_cities))
+        tour = [0]  # Start from the first city (city 0)
+        unvisited_cities.remove(0)
+        total_distance = 0
 
-def nearest_neighbor_tsp(cities):
-    start_city = cities[0]
-    start_city.visited = True
-    tour = [start_city]
-    total_distance = 0  # To keep track of the total distance
+        while unvisited_cities:
+            current_city = tour[-1]
+            nearest_city = min(unvisited_cities, key=lambda city: self.distance_matrix[current_city][city])
+            tour.append(nearest_city)
+            total_distance += self.distance_matrix[current_city][nearest_city]
+            unvisited_cities.remove(nearest_city)
 
-    while len(tour) < len(cities):
-        current_city = tour[-1]
-        min_distance = float('inf')
-        nearest_city = None
+        # Return to the starting city to complete the tour
+        tour.append(0)
+        total_distance += self.distance_matrix[tour[-2]][0]
 
-        for city in cities:
-            if not city.visited:
-                distance = current_city.distance_to(city)
-                if distance < min_distance:
-                    min_distance = distance
-                    nearest_city = city
-
-        nearest_city.visited = True
-        tour.append(nearest_city)
-        total_distance += min_distance
-
-    # Return to the starting city to complete the tour
-    tour.append(start_city)
-    total_distance += start_city.distance_to(tour[-2])  # Add the distance back to the starting city
-
-    return tour, total_distance
+        return tour, total_distance
 
 # Example usage:
-city1 = City("A", 0, 0)
-city2 = City("B", 15, 9)
-city3 = City("C", 14, 5)
-city4 = City("D", 10, 7)
+distance_matrix = [
+    [0, 10, 15, 20],
+    [10, 0, 35, 25],
+    [15, 35, 0, 30],
+    [20, 25, 30, 0]
+]
 
-cities = [city1, city2, city3, city4]
-optimal_tour, shortest_distance = nearest_neighbor_tsp(cities)
+tsp = TravelingSalesman(distance_matrix)
+optimal_tour, shortest_distance = tsp.nearest_neighbor_tsp()
 
 print("Optimal tour order:")
-for city in optimal_tour:
-    print(city.name)
-
-print(f"Shortest distance: {shortest_distance:.2f}")
+print(optimal_tour)
+print(f"Shortest distance: {shortest_distance}")
